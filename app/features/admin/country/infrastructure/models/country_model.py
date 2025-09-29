4f203f0bc01ea00d86d23eef4592c16095a4c8c3
Richard Dewan <richarddewan@hotmail.com>
2025-09-29 06:44:30 +0530

    create a country entity

--- app/features/admin/country/infrastructure/models/country_model.py
+++ app/features/admin/country/infrastructure/models/country_model.py
@@ -7,10 +7,10 @@ from sqlalchemy.orm import func
 class CountryModel(Base):
     __tablename__ = "countries"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True, nullable=False)
-    code = Column(String, unique=True, index=True, nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    country_code = Column(String, unique=True, nullable=False)
     currency_code = Column(String, nullable=False)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
