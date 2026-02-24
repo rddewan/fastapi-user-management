9c4f8c3ac7b8cffcd39d578e706ec429b2a4f84f
Richard Dewan <richarddewan@hotmail.com>
2026-02-24 07:56:51 +0530

    schema validator

--- app/features/shared/user/interface/schemas.py
+++ app/features/shared/user/interface/schemas.py
@@ -1,20 +1,40 @@
 
 
-
+import re
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field, field_validator, model_validator
 
 from app.features.shared.user.domain.user_entity import UserEntity
 
 
 class CreateUserRequest(BaseModel):
-    username: str
-    email: str
-    password: str
-    password_confirmation: str 
-    full_name: Optional[str]
-    phone: Optional[str]
-    is_active: Optional[str]
+    username: str = Field(..., min_length=3, max_length=20)
+    email: EmailStr
+    password: str = Field(..., min_length=8, max_length=20)
+    password_confirmation: str = Field(..., min_length=8, max_length=20)
+    full_name: Optional[str] = None
+    phone: Optional[str] = None
+    is_active: Optional[str] = None
+    
+    @field_validator("password")
+    @classmethod
+    def validate_password_streangth(cls, value: str) -> str:
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("password must contain at least one uppercase letter")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("password must contain at least one lowercase letter")
+        if not re.search(r"[\d]", value):
+            raise ValueError("password must contain at least one numner")
+        if not re.search(r"[^A-Za-z0-9, value]", value):
+            raise ValueError("password must contain at least one special character")
+        
+        return value
+    
+    @model_validator(mode="after")
+    def validate_passwoed_confirmation(self) -> "CreateUserRequest":
+        if self.password != self.password_confirmation:
+            raise ValueError("password and password_confirmation do not match")
+        return self
     
 
 class UpdateUserRespone(BaseModel):
@@ -25,13 +45,47 @@ class UpdateUserRespone(BaseModel):
 
 class UpdateEmailRequest(BaseModel):
     email: str
-    current_password: str
+    current_password: str = Field(..., min_length=8, max_length=20)
+    
+    @field_validator("current_password")
+    @classmethod
+    def validate_password_streangth(cls, value: str) -> str:
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("password must contain at least one uppercase letter")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("password must contain at least one lowercase letter")
+        if not re.search(r"[\d]", value):
+            raise ValueError("password must contain at least one numner")
+        if not re.search(r"[^A-Za-z0-9, value]", value):
+            raise ValueError("password must contain at least one special character")
+        
+        return value
     
 
 class UpdatePasswordRequest(BaseModel):
-    current_password: str 
-    new_password: str
-    new_password_confirmation: str
+    current_password: str = Field(..., min_length=8, max_length=20)
+    new_password: str = Field(..., min_length=8, max_length=20)
+    new_password_confirmation: str = Field(..., min_length=8, max_length=20)
+    
+    @field_validator("new_password")
+    @classmethod
+    def validate_password_streangth(cls, value: str) -> str:
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("password must contain at least one uppercase letter")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("password must contain at least one lowercase letter")
+        if not re.search(r"[\d]", value):
+            raise ValueError("password must contain at least one numner")
+        if not re.search(r"[^A-Za-z0-9, value]", value):
+            raise ValueError("password must contain at least one special character")
+        
+        return value
+    
+    @model_validator(mode="after")
+    def validate_passwoed_confirmation(self) -> "UpdatePasswordRequest":
+        if self.new_password != self.new_password_confirmation:
+            raise ValueError("password and password_confirmation do not match")
+        return self
 
 
 class PaginationMeta(BaseModel):
